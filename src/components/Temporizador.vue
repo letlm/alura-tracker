ee89d242100ffd068296a3c7065661dfe77bc5ae
letlm <lealmleticia@gmail.com>
2023-02-09 08:54:57 -0300

    refactor: separação do botão do temporizador 📦

--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -1,27 +1,30 @@
 <template>
   <div class="is-flex is-align-items-center is-justify-content-space-between">
     <Cronometro :tempoEmSegundos="tempoEmSegundos" />
-    <button class="button" @click="iniciar" :disabled="cronometroRodando">
-      <span class="icon">
-        <i class="fas fa-play"></i>
-      </span>
-      <span>play</span>
-    </button>
-    <button class="button" @click="finalizar" :disabled="!cronometroRodando">
-      <span class="icon">
-        <i class="fas fa-stop"></i>
-      </span>
-      <span>stop</span>
-    </button>
+    <Botao
+      @clicado="iniciar"
+      icone="fas fa-play"
+      texto="play"
+      :disabled="cronometroRodando"
+    />
+    <Botao
+      @clicado="finalizar"
+      icone="fas fa-stop"
+      texto="stop"
+      :disabled="!cronometroRodando"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Botao from "./Botao.vue";
 import Cronometro from "./Cronometro.vue";
 
 export default defineComponent({
   name: "Temporizador",
+  emits: ["aoTemporizadorFinalizado"],
+  components: { Cronometro, Botao },
   data() {
     return {
       tempoEmSegundos: 0,
@@ -39,8 +42,9 @@ export default defineComponent({
     finalizar() {
       this.cronometroRodando = false;
       clearInterval(this.cronometro);
+      this.$emit("aoTemporizadorFinalizado", this.tempoEmSegundos);
+      this.tempoEmSegundos = 0;
     },
   },
-  components: { Cronometro },
 });
 </script>
