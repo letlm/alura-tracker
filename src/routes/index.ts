b260b25d1e7adca66c4b7347ffc26044c028c0ba
letlm <lealmleticia@gmail.com>
2023-02-12 16:39:47 -0300

    feat: inserção das views para rotas ✨

---
+++ src/routes/index.ts
@@ -0,0 +1,22 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import Tarefas from "../views/Tarefas.vue";
+
+const rotas: RouteRecordRaw[] = [
+  {
+    path: "/",
+    name: "Tarefas",
+    component: Tarefas,
+  },
+  {
+    path: "",
+    name: "",
+    component: Tarefas,
+  },
+];
+
+const routes = createRouter({
+  history: createWebHashHistory(),
+  routes: rotas,
+});
+
+export default routes;
