042f1c47f43f3826b9399fb5764fbfec2c1c9232
letlm <lealmleticia@gmail.com>
2023-02-13 08:47:35 -0300

    refactor: reestruturando view de Projetos e separando responsabilidades 📦

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Tarefas from "../views/Tarefas.vue";
 import Projetos from "../views/Projetos.vue";
+import Formulario from "../views/Projetos/Formulario.vue";
+
 const rotas: RouteRecordRaw[] = [
   {
     path: "/",
@@ -11,6 +13,16 @@ const rotas: RouteRecordRaw[] = [
     path: "/projetos",
     name: "Projetos",
     component: Projetos,
+  },
+  {
+    path: "/projetos/novo",
+    name: "Novo projeto",
+    component: Formulario,
+  },
+  {
+    path: "/projetos/:id",
+    name: "Editar projeto",
+    component: Formulario,
   },
 ];
 
