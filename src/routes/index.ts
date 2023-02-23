1db6e20c86d7914611defaff2add166cc177c6c9
letlm <lealmleticia@gmail.com>
2023-02-23 08:45:52 -0300

    feat: inserção da possibilidade de deletar e editar os projetos ✨

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Tarefas from "../views/Tarefas.vue";
 import Projetos from "../views/Projetos.vue";
 import Formulario from "../views/Projetos/Formulario.vue";
+import Lista from "../views/Projetos/Lista.vue";
 
 const rotas: RouteRecordRaw[] = [
   {
@@ -9,21 +10,28 @@ const rotas: RouteRecordRaw[] = [
     name: "Tarefas",
     component: Tarefas,
   },
-  {
-    path: "/projetos",
-    name: "Projetos",
-    component: Projetos,
-  },
-  {
-    path: "/projetos/novo",
-    name: "Novo projeto",
-    component: Formulario,
-  },
-  {
-    path: "/projetos/:id",
-    name: "Editar projeto",
-    component: Formulario,
-  },
+ {
+  path: "/projetos",
+  component: Projetos,
+  children: [
+    {
+      path: "",
+      name: "Projetos",
+      component: Lista,
+    },
+    {
+      path: "novo",
+      name: "Novo projeto",
+      component: Formulario,
+    },
+    {
+      path: ":id",
+      name: "Editar projeto",
+      component: Formulario,
+      props: true
+    },
+  ]
+ }
 ];
 
 const routes = createRouter({
--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -1,6 +1,5 @@
 <template>
-  <section class="projetos">
-    <h1 class="title">Projetos</h1>
+  <section>
     <form @submit.prevent="salvar">
       <div class="field">
         <label for="nomeDoProjeto" class="label">Nome do Projeto</label>
@@ -20,32 +19,56 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
+import useNotificador from "@/hooks/notificador";
 import { useStore } from "@/store";
+import { ADICIONA_PROJETO, ALTERA_PROJETO } from "@/store/tipo-mutacoes";
+import { TipoNotificacao } from "@/interfaces/INotificacao";
 
 export default defineComponent({
   name: "Formulario",
+  props: {
+    id: {
+      type: String,
+    },
+  },
+
+  mounted() {
+    if (this.id) {
+      const projeto = this.store.state.projetos.find(
+        (proj) => proj.id == this.id
+      );
+      this.nomeDoProjeto = projeto?.nome || "";
+    }
+  },
   data() {
     return { nomeDoProjeto: "" };
   },
   methods: {
     salvar() {
-      this.store.commit("ADICIONA_PROJETO", this.nomeDoProjeto);
+      if (this.id) {
+        this.store.commit(ALTERA_PROJETO, {
+          id: this.id,
+          nome: this.nomeDoProjeto,
+        });
+      } else {
+        this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto);
+      }
       this.nomeDoProjeto = "";
+      this.notificar(
+        TipoNotificacao.SUCESSO,
+        "Excelente",
+        "O projeto foi cadastrado com sucesso!"
+      );
       this.$router.push("/projetos");
     },
   },
   setup() {
     const store = useStore();
+    const { notificar } = useNotificador();
     return {
       store,
+      notificar,
     };
   },
 });
 </script>
-
-<style scoped>
-.projetos {
-  padding: 1.25rem;
-}
-</style>
